b7049a1f64b444ad103291e19a4a82fa8a2509f2
Hildura <hilda-mari.myllymaki@student.hamk.fi>
2023-10-18 15:41:50 +0300

    Starting coding

---
+++ LuckySevenGame.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LuckySeven {
+    public static void main(String[] args) {
+
+        String answer = "";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give me money, "
+                + " how much do you want to play?");
+        int money = scan.nextInt();
+          do {}
+        while:
